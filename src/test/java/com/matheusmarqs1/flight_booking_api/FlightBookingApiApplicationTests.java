9283e0e32cf13bdadc3aa65b09e0be86389323af
Matheus Teles <matheusti770@gmail.com>
2025-08-15 15:36:47 -0300

    Project created

---
+++ src/test/java/com/matheusmarqs1/flight_booking_api/FlightBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmarqs1.flight_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
